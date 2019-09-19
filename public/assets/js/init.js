c632f8877c541496363ad5240ad08aa6fe42a6cf
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-19 00:11:57 -0700

    added dicover page and carousel

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,8 +1,22 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+
   }); // end of document ready
+  //carousel
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: false,
+    dist: -100
+
+  });
+
 })(jQuery); // end of jQuery name space
+
+$('.carousel.carousel-slider').carousel({ fullWidth: true, indicators: true });
+var autoplay = true;
+setInterval(function () { if (autoplay) $('.carousel.carousel-slider').carousel('next'); }, 4500);
+$('.carousel.carousel-slider').hover(function () { autoplay = false; }, function () { autoplay = true; });
